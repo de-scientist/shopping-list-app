0122d8a7b207aaf78dbdafc72fa1a04c5db264ec
de-scientist <gitaumark502@gmail.com>
2025-11-12 19:12:23 +0300

    use the shoppingApp component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ShoppingApp from "./components/Shopping";
 import "./index.css";
 
+//use the shoppingApp component
 const App: React.FC = () => <ShoppingApp />;
 
 export default App;
